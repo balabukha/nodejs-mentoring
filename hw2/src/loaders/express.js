d827777e6a6ea302ce2ae7abed4b4533378b740d
Andrey Balabukha <5344208@gmail.com>
2020-03-01 23:58:35 +0300

    [hw-005] add error handling

--- hw2/src/loaders/express.js
+++ hw2/src/loaders/express.js
@@ -3,13 +3,22 @@ import * as bodyParser from 'body-parser';
 import UserRouts from '../users/UserRouts';
 import GroupRouts from '../group/GroupRouts';
 import UserGroupRouts from '../userGroup/UserGroupRouts';
+import requestLogger from '../middlewares/requestLogger';
 
 export default async ({ app }) => {
     app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use('*', requestLogger);
     app.use('/users', UserRouts);
     app.use('/groups', GroupRouts);
     app.use('/user-groups', UserGroupRouts);
-
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use('*', async (req, res) => {
+        res.status(404);
+        res.end('404');
+    });
+    app.use((err, req, res) => {
+        console.error(err.stack);
+        res.status(500).send(`Server error ${err}`);
+    });
     return app;
 };
